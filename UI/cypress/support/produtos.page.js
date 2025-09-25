da4b6776cda37d409a738ad13cdc190dfed090dc
Marcio Filho <marcio.dellatorre@hotmail.com>
2025-09-25 16:39:43 -0300

    Teste UI

---
+++ UI/cypress/support/produtos.page.js
@@ -0,0 +1,21 @@
+class produtosPage{
+    visitarUrl(){
+        cy.visit('produtos')
+    }
+
+    buscarProduto(nomeProduto){
+        cy.get('[name="s"]').eq(1).type(nomeProduto)
+        cy.get('.button-search').eq(1).click()
+    }
+
+    addProdutoCarriho(tamanho, cor, quantidade){
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get('.button-variable-item-' + cor).click()
+        for (let index = 1; index < quantidade; index++) {
+            cy.get('.plus').click()
+        }        
+        cy.get('.single_add_to_cart_button').click()
+        
+    }
+}
+export default new produtosPage()
